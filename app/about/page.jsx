814abc72d4f7b02243ffa26891184b121983dc87
gokulprasanthb <gokulprasanthbm@gmail.com>
2024-07-13 14:16:31 +0530

    about page added

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className="container mx-auto px-4 py-6">
+        <h2 className="text-4xl font-bold mb-4">About Us</h2>
+        <p>BlogSpace, a space dedicated to exploring, sharing, and celebrating. Our mission is to inspire, inform, and engage our readers through high-quality content that resonates with their interests and passions.</p>
+    </main>
+  )
+}
+
+export default page
--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
    <header>
       <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-                    <Link href="/"><h1 className="text-2xl">Blog</h1></Link>
+                    <Link href="/"><h1 className="text-2xl font-semibold">BlogSpace</h1></Link>
                     <nav className="space-x-4">
                         <Link href="/" className="text-blue-500">Home</Link>
                         <Link href="/about" className="text-blue-500">About</Link>
