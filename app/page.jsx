a0c9e39eb4ef62ea47ea14f51ce69a769b899ef3
gokulprasanthb <gokulprasanthbm@gmail.com>
2024-07-14 10:39:18 +0530

    posts section added

--- app/page.jsx
+++ app/page.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+"use client"
+import { useState, useRef, useEffect} from "react";
+import { FaSearch } from "react-icons/fa";
+
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([]);
+  const inputRef = useRef("");
+  const [search, setSearch] = useState(false);
+
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_API_URL+'/posts')
+    .then((res) => res.json() )
+    .then(res => setPosts(res))
+  },[]);
+
   return (
-    <section>
+    <section className="my-10 px-20">
     <main className="container mx-auto px-4 py-6">
       <h2 className="text-4xl font-bold mb-4">Welcome to Our Blog</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-  </main>
-  <div className="flex justify-end px-4">
+    </main>
+    <div className="flex justify-end px-4">
       <input type="text" className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
-      <button className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">Search</button>
-  </div>
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      <div className="border border-gray-200 p-4">
-        <img className="w-full h-48 object-cover mb-4" src="https://picsum.photos/200" alt="Post Image"/>
-        <h2 className="text-xl font-semibold mb-2">Post Title 1</h2>
-        <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-      <div className="border border-gray-200 p-4">
-        <img className="w-full h-48 object-cover mb-4" src="https://picsum.photos/200" alt="Post Image"/>
-        <h2 className="text-xl font-semibold mb-2">Post Title 2</h2>
-        <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-      <div className="border border-gray-200 p-4">
-        <img className="w-full h-48 object-cover mb-4" src="https://picsum.photos/200" alt="Post Image"/>
-        <h2 className="text-xl font-semibold mb-2">Post Title 3</h2>
-        <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-      {/* <!-- Add more posts here --> */}
-  </div>
+      <button className="px-4 py-2 bg-violet-500 text-white rounded-md ml-4"><FaSearch /></button>
+    </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-10">
+      {posts.map((post) => (<div className="border border-gray-200 p-4">
+        <img className="w-full h-48 object-cover mb-4" src={post.image} alt="Post Image"/>
+        <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+        <p className="text-gray-400">{post.description}</p>
+      </div>)
+      )}
+    </div>
  </section>
   )
 }
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     const year = new Date();
   return (
-    <footer className="fixed inset-x-0 bottom-0 flex py-10 flex-col items-center bg-none text-center text-white">
+    <footer className="inset-x-0 bottom-0 flex py-10 flex-col items-center bg-none text-center text-white">
                 <div className="w-full p-4 text-center" style={{backgroundColor:'rgba(0, 0, 0, 0.2)'}}>
                 Copyright &copy; {year.getFullYear()} BlogSpace | All Rights Reserved
                 </div>
