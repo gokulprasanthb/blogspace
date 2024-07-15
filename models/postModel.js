eefce93e54345ca58daf21340088e36bc4ad4e3c
gokulprasanthb <gokulprasanthbm@gmail.com>
2024-07-15 14:32:19 +0530

    post page updated

--- models/postModel.js
+++ models/postModel.js
@@ -10,9 +10,17 @@ const postSchema =  new Schema({
 postSchema.virtual('small_description').get(function() {
     return this.description.substr(0,100)+'...'
 });
-// postSchema.virtual('created_at').get(function() {
-//     return changeDateFormat(this.created_at)
-// });
+postSchema.virtual('created_at_anotherFormat').get(function() {
+    return changeDateFormat(this.created_at)
+});
+
+function changeDateFormat(date_str) {
+    const date = new Date(date_str);
+    const months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+    ];
+    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
 
 const PostModel = models.Post || model('Post', postSchema);
 
